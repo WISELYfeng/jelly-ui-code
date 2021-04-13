e6c96e52ecbbd1b7990773b0179721094ebb2d96
wsielyfeng <learnroad@163.com>
2021-04-13 20:54:12 +0800

    增加移动端菜单切换按钮

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleAside">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"></span>
+    <span class="toggleAside" @click="toggleAside">
+    </span>
   </div>
 </template>
 <script lang="ts">
@@ -41,10 +42,21 @@ export default {
       margin: 0 1em;
     }
   }
-  > .toggleAside{}
+  > .toggleAside{
+    display: none;
+    width: 24px;
+    height: 24px;
+    background: url(../assets/menu.png) center center no-repeat;
+    background-size: 24px;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
   @media (max-width:500px) {
     > .menu{display:none}
     > .logo{margin:0 auto}
+    > .toggleAside{ display: inline-block;}
   }
 }
 </style>
