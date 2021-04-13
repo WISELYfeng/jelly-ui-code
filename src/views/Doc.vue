21ca49115da47bb114e1cbf6287551cf12dfa284
wsielyfeng <learnroad@163.com>
2021-04-13 21:00:48 +0800

    完善aside切换逻辑

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,41 +1,41 @@
 <template>
-    <div>
-        <Topnav></Topnav>
-        <div class="content">
-            <aside v-if="asideVisible">
-                <h2>组件列表</h2>
-                <ol>
-                    <li>
-                        <router-link to="/doc/swich">Swich组件</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/doc/button">Buton组件</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/doc/dialog">Dialog组件</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/doc/tabs">Tabs组件</router-link>
-                    </li>
-                </ol>
-            </aside>
-            <main>主内容</main>
-        </div>
+  <div>
+    <Topnav></Topnav>
+    <div class="content">
+      <aside v-if="asideVisible">
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/swich">Swich组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Buton组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs组件</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main>主内容</main>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { inject, Ref } from 'vue';
-import Topnav from '../components/Topnav.vue'
+import { inject, Ref } from "vue";
+import Topnav from "../components/Topnav.vue";
 
-export default{
-    components:{
-        Topnav
-    },
-    setup(){
-        const asideVisible = inject<Ref<boolean>>('asideVisible')
-        return {asideVisible}
-    }
+export default {
+  components: {
+    Topnav,
+  },
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("asideVisible");
+    return { asideVisible };
+  },
 };
 </script>
 
@@ -45,10 +45,7 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top:70px;
+
   > h2 {
     margin-bottom: 4px;
   }
@@ -56,6 +53,12 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+  @media (max-width: 500px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
   }
 }
 </style> 
