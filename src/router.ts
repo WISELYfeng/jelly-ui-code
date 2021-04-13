d0af14c80c00512a7173eabf2e7a06851d223b12
wsielyfeng <learnroad@163.com>
2021-04-13 23:00:08 +0800

    完成官网骨架搭建

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,10 @@ import {createWebHashHistory,createRouter} from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import Switch from './components/Switch.vue'
+import Button from './components/Button.vue'
+import Dialog from './components/Dialog.vue'
+import Tabs from './components/Tabs.vue'
+import DocDemo from './components/Doc.vue'
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -13,7 +17,11 @@ export const router = createRouter({
         {
             path:'/doc',component:Doc,
             children:[
-                {path:'switch',component:Switch}
+                {path:'',component:DocDemo},
+                {path:'switch',component:Switch},
+                {path:'button',component:Button},
+                {path:'dialog',component:Dialog},
+                {path:'tabs',component:Tabs}
             ]
         }
     ]
