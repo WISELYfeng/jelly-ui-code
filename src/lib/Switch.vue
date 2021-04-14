50bc5633bfda8d1789d03db109073e51d4043123
wsielyfeng <learnroad@163.com>
2021-04-14 22:43:47 +0800

    优化button动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -39,6 +39,7 @@ export default {
         width:$h2;
         background:white;
         border-radius: $h2 /2;
+        transition:left .3s;
     }
     button.checked {
         background:#1989fa;
@@ -46,5 +47,7 @@ export default {
     button.checked > span {
         left:calc(100% - #{$h2} - 2px);
     }
-
+    button:focus{
+        outline: none;
+    }
 </style>
