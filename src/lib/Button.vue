ae4edfbae0ef7b77c6592fd8b73e3ea2e654f26f
wsielyfeng <learnroad@163.com>
2021-04-15 22:55:03 +0800

    支持theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -52,5 +52,21 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.jelly-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,&:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.jelly-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,&:focus{
+      background: darken(white, 5%);;
+    }
+  }
 }
 </style>
