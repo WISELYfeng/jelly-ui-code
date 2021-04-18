19afcec158ebceccabbafd2d54b0ef8b03a171c5
wsielyfeng <learnroad@163.com>
2021-04-18 21:56:00 +0800

    参数式渲染Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,28 +1,29 @@
 <template>
-<div style="position: relative; z-index: 1;">
-
-  <Button @click="toggle">toggle</Button>
-  <Dialog
-    v-model:visible="visible"
-    :closeOnClickOverlay="false"
-    :ok="f1"
-    :cancel="f2"
-  >
-  <template v-slot:content>
-    <strong>hi</strong>
-    <div>hi2</div>
-  </template>
-  <template v-slot:title>
-    <strong>加粗的标题</strong>
-  </template>
-  </Dialog>
-</div>
+  <div style="position: relative; z-index: 1">
+    <Button @click="toggle">toggle</Button>
+    <Dialog
+      v-model:visible="visible"
+      :closeOnClickOverlay="false"
+      :ok="f1"
+      :cancel="f2"
+    >
+      <template v-slot:content>
+        <strong>hi</strong>
+        <div>hi2</div>
+      </template>
+      <template v-slot:title>
+        <strong>加粗的标题</strong>
+      </template>
+    </Dialog>
+  </div>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from "../lib/Dialog.vue";
 import Button from "../lib/Button.vue";
-import { ref } from "vue";
+import { ref, h } from "vue";
+import { openDialog } from "../lib/openDialog";
 export default {
   components: {
     Dialog,
@@ -37,11 +38,24 @@ export default {
       return false;
     };
     const f2 = () => {};
+    const showDialog = () => {
+      openDialog({
+        title: h('strong', {}, '标题'),
+        content: '你好',
+        ok() {
+          console.log('ok')
+        },
+        cancel() {
+          console.log('cancel')
+        }
+      })
+    }
     return {
       visible,
       toggle,
       f1,
-      f2
+      f2,
+      showDialog
     };
   },
 };
