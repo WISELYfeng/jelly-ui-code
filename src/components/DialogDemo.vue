d2a0d2a4056832d68c0791abf730ea4175483147
wsielyfeng <learnroad@163.com>
2021-04-18 20:58:46 +0800

    设置dialog样式

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    Dialog示例
-  </div>
+  <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {  
+  components:{
+    Dialog
+  }
+}
+</script>
