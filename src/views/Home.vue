09201b30333227008ac0df1ee62b597fab16969a
wsielyfeng <learnroad@163.com>
2021-04-19 23:55:39 +0800

    修改首页背景和引入icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="top-wrapper">
-    <Topnav></Topnav>
-    <div class="banner">
-      <h1>果冻UI</h1>
-      <h2>一个简洁易用的UI框架</h2>
-      <p class="actions">
-        <a href="https://github.com/WISELYfeng/vite-web">Github</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+  <div>
+    <div class="top-wrapper">
+      <Topnav></Topnav>
+      <div class="banner">
+        <h1>果冻UI</h1>
+        <h2>一个简洁易用的UI框架</h2>
+        <p class="actions">
+          <a href="https://github.com/WISELYfeng/vite-web">Github</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+
+    <div class="features">
+      <svg class="icon">
+        <use xlink:href="#icon-Vue"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-ts"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon--JellyFlat"></use>
+      </svg>
     </div>
   </div>
 </template>
@@ -24,10 +38,27 @@ export default {
 
 
 <style lang="scss" scoped>
-.top-wrapper{
-  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+
+.top-wrapper {
+  background: linear-gradient(
+    145deg,
+    rgba(227, 255, 253, 1) 0%,
+    rgba(183, 233, 230, 1) 100%
+  );
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
+.features {
+  >svg {
+    width: 64px;
+    height: 64px;
+  }
+}
+
 .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
@@ -37,13 +68,14 @@ export default {
     padding: 8px 0;
     a {
       margin: 0 8px;
-      background: #fff;
+      background: $green;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      color:white;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
